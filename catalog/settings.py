a014775407aacac0b58cfdb688511c6e12e5efba
rapplayerr <wageroffender@gmail.com>
2026-03-12 22:53:45 +0300

    изменен файл сеттингс для деплоя с помощью вм

--- catalog/settings.py
+++ catalog/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ahpuxps(^lza+jnxr8320%w_00f=-%d_gh9d8!a&9cx-d7=yr'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-4ahpuxps(^lza+jnxr8320%w_00f=-%d_gh9d8!a&9cx-d7=yr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
